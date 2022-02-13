84040bed6b9f5e8d31b2f3aff18cb474c2659dfa
DarkBird <darkbird@vivaldi.net>
2022-02-13 13:37:27 +0100

    cmake test

---
+++ build/CMakeFiles/editor.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/darkbird/projects/owd-editor/src/app.cpp" "/home/darkbird/projects/owd-editor/build/CMakeFiles/editor.dir/src/app.cpp.o"
+  "/home/darkbird/projects/owd-editor/src/compositionDialog.cpp" "/home/darkbird/projects/owd-editor/build/CMakeFiles/editor.dir/src/compositionDialog.cpp.o"
+  "/home/darkbird/projects/owd-editor/src/edits.cpp" "/home/darkbird/projects/owd-editor/build/CMakeFiles/editor.dir/src/edits.cpp.o"
+  "/home/darkbird/projects/owd-editor/src/history.cpp" "/home/darkbird/projects/owd-editor/build/CMakeFiles/editor.dir/src/history.cpp.o"
+  "/home/darkbird/projects/owd-editor/src/imagePanel.cpp" "/home/darkbird/projects/owd-editor/build/CMakeFiles/editor.dir/src/imagePanel.cpp.o"
+  "/home/darkbird/projects/owd-editor/src/main.cpp" "/home/darkbird/projects/owd-editor/build/CMakeFiles/editor.dir/src/main.cpp.o"
+  "/home/darkbird/projects/owd-editor/src/mainFrame.cpp" "/home/darkbird/projects/owd-editor/build/CMakeFiles/editor.dir/src/mainFrame.cpp.o"
+  "/home/darkbird/projects/owd-editor/src/regionPanel.cpp" "/home/darkbird/projects/owd-editor/build/CMakeFiles/editor.dir/src/regionPanel.cpp.o"
+  "/home/darkbird/projects/owd-editor/src/toolbar.cpp" "/home/darkbird/projects/owd-editor/build/CMakeFiles/editor.dir/src/toolbar.cpp.o"
+  "/home/darkbird/projects/owd-editor/src/wxNumberCtrl.cpp" "/home/darkbird/projects/owd-editor/build/CMakeFiles/editor.dir/src/wxNumberCtrl.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "WXUSINGDLL"
+  "_FILE_OFFSET_BITS=64"
+  "__WXGTK__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/lib/wx/include/gtk3-unicode-3.1"
+  "/usr/local/include/wx-3.1"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
